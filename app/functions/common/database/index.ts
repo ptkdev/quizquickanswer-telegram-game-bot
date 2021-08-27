aebe96635c229485d72df721172fb4ac37cda915
Patryk Rzucidlo (PTKDev) <ptkdev@gmail.com>
2021-08-27 17:03:09 +0200

    [Fix] Prettier

--- app/functions/common/database/index.ts
+++ app/functions/common/database/index.ts
@@ -2,34 +2,31 @@
 import configs from "../../../configs/config";
 import Mongoose from "mongoose";
 
-
 let database: Mongoose.connection;
 
 /**
  * MongoDB Connection
  * =====================
- * Connects to mongo DB 
+ * Connects to mongo DB
  *
  */
 export const connectDB = async (): Promise<void> => {
-    if (database) {
-        return;
-    }
-    try {
-        await Mongoose.connect(configs.database.URL, {
-            useNewUrlParser: true,
-            useFindAndModify: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-        });
-        database = Mongoose.connection;
-        console.log("Connected to database");
-
-    } catch (err) {
-        console.log("Failed to connect to MongoDB - ", err);
-    }
+	if (database) {
+		return;
+	}
+	try {
+		await Mongoose.connect(configs.database.URL, {
+			useNewUrlParser: true,
+			useFindAndModify: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		});
+		database = Mongoose.connection;
+		console.log("Connected to database");
+	} catch (err) {
+		console.log("Failed to connect to MongoDB - ", err);
+	}
 };
-
 
 /**
  * MongoDB Disconnection
@@ -38,15 +35,13 @@ export const connectDB = async (): Promise<void> => {
  *
  */
 export const disconnectDB = async (): Promise<void> => {
-    if (!database) {
-        return;
-    }
-    try {
-        Mongoose.disconnect();
-        console.log("Disconnected from database");
-    } catch (err) {
-        console.log("Failed to disconnect from MongoDB - ", err);
-    }
-
+	if (!database) {
+		return;
+	}
+	try {
+		Mongoose.disconnect();
+		console.log("Disconnected from database");
+	} catch (err) {
+		console.log("Failed to disconnect from MongoDB - ", err);
+	}
 };
-
--- app/functions/common/database/users.ts
+++ app/functions/common/database/users.ts
@@ -3,27 +3,27 @@ import { Schema, model } from "mongoose";
 import type { TelegramUserInterface } from "../../../types/databases.type";
 
 const schema = new Schema<TelegramUserInterface>({
-    id: { type: String, required: true },
-    is_bot: { type: Boolean, required: true },
-    first_name: { type: String, required: true },
-    username: { type: String, required: true },
-    launguage_code: String,
+	id: { type: String, required: true },
+	is_bot: { type: Boolean, required: true },
+	first_name: { type: String, required: true },
+	username: { type: String, required: true },
+	launguage_code: String,
 });
 
 const user_model = model<TelegramUserInterface>("User", schema);
 
 /**
- * Users CRUD 
+ * Users CRUD
  * =====================
  * Add user to DB
  *
  * @param {TelegramUserInterface} user - user to add
  */
 export const addUser = async (user: TelegramUserInterface): Promise<void> => {
-    const doc = new user_model(user);
-    await doc.save();
+	const doc = new user_model(user);
+	await doc.save();
 
-    console.log("User Created");
+	console.log("User Created");
 };
 
 /**
@@ -34,13 +34,12 @@ export const addUser = async (user: TelegramUserInterface): Promise<void> => {
  * @param {number } id - user id to remove
  */
 export const deleteUser = async (id: number): Promise<void> => {
-    user_model.findOneAndDelete({ id }, function (err, user) {
-        if (err) {
-            return err;
-        }
-        console.log("User deleted");
-
-    });
+	user_model.findOneAndDelete({ id }, function (err, user) {
+		if (err) {
+			return err;
+		}
+		console.log("User deleted");
+	});
 };
 
 /**
@@ -52,13 +51,12 @@ export const deleteUser = async (id: number): Promise<void> => {
  * @param {TelegramUserInterface} user - user info to update
  */
 export const updateUser = async (id: number, user: TelegramUserInterface): Promise<void> => {
-    user_model.findOneAndUpdate({ id }, user, function (err, user) {
-        if (err) {
-            return err;
-        }
-        console.log(`User updated`);
-
-    });
+	user_model.findOneAndUpdate({ id }, user, function (err, user) {
+		if (err) {
+			return err;
+		}
+		console.log(`User updated`);
+	});
 };
 
 /**
@@ -69,13 +67,10 @@ export const updateUser = async (id: number, user: TelegramUserInterface): Promi
  * @param {number } id - user id to retrieve
  */
 export const getUser = async (id: number): Promise<void> => {
-    const user = await user_model.findOne({ id }, function (err, user) {
-        if (err) {
-            return err;
-        }
-    });
-    console.log(user);
+	const user = await user_model.findOne({ id }, function (err, user) {
+		if (err) {
+			return err;
+		}
+	});
+	console.log(user);
 };
-
-
-
