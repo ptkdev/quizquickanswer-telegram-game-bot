49e023a53fe4918494bd2f7a8943a47fb5370657
Patryk Rzucidlo (PTKDev) <ptkdev@gmail.com>
2021-12-26 01:17:01 +0100

    [CI/CD] Github workflow

---
+++ scripts/changelog_release.ts
@@ -0,0 +1,28 @@
+/**
+ * Version
+ * =====================
+ * Increment package.json version
+ *
+ * @contributors: Patryk Rzucidło [@ptkdev] <support@ptkdev.io> (https://ptk.dev)
+ *
+ * @license: MIT License
+ *
+ */
+import * as fs from "fs";
+
+if (fs.existsSync("./CHANGELOG.md")) {
+	fs.readFile("./CHANGELOG.md", "utf8", (err, data) => {
+		if (err) {
+			console.log(err);
+		}
+
+		const changelog = data.match(/\n([\s\S]*)-->\n/gm);
+		changelog?.forEach((c) => {
+			fs.writeFile("./CHANGELOG_RELEASE.txt", c, function writeJSON(err) {
+				if (err) {
+					console.log(err);
+				}
+			});
+		});
+	});
+}
---
+++ scripts/version.ts
@@ -0,0 +1,51 @@
+/**
+ * Version
+ * =====================
+ * Increment package.json version
+ *
+ * @contributors: Patryk Rzucidło [@ptkdev] <support@ptkdev.io> (https://ptk.dev)
+ *
+ * @license: MIT License
+ *
+ */
+import * as fs from "fs";
+import { argv } from "yargs";
+import pkg from "../package.json";
+
+const version = pkg.version.split(".");
+let next_version, patch;
+
+switch (argv.cmd) {
+	case "nightly":
+		next_version = `${version[0]}.${version[1]}.${version[2]}.${Number(version[3]) + 1}`;
+		break;
+
+	case "nightly-next":
+		patch = version[2].split("-");
+		next_version = `${version[0]}.${version[1]}.${Number(patch[0]) + 1}-nightly.0`;
+		break;
+
+	case "beta":
+		patch = version[2].split("-");
+		next_version = `${version[0]}.${version[1]}.${Number(patch[0])}-beta.1`;
+		break;
+
+	case "main":
+		patch = version[2].split("-");
+		next_version = `${version[0]}.${version[1]}.${Number(patch[0])}`;
+		break;
+
+	default:
+		next_version = "0.0.0";
+		break;
+}
+
+pkg.version = next_version;
+
+if (fs.existsSync("./package.json")) {
+	fs.writeFile("./package.json", JSON.stringify(pkg), function writeJSON(err) {
+		if (err) {
+			console.log(err);
+		}
+	});
+}
